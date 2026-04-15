356e5eec23bf769f4b32ae405acffd9fed6bbb5e
Cynthia Milan <milan.giovanna58@gmail.com>
2026-04-14 18:52:23 -0700

    add EC2 instance and security group

--- main.tf
+++ main.tf
@@ -19,3 +19,51 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "dev"
   }
 }
+
+resource "aws_security_group" "app_sg" {
+  name        = "health-app-sg"
+  description = "Allow web and SSH traffic"
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y docker
+    service docker start
+    docker run -d -p 5000:5000 animeluverx97/my-health-app
+  EOF
+
+  tags = {
+    Name = "health-app-server"
+  }
+}
+
+output "app_url" {
+  value = "http://${aws_instance.app_server.public_ip}:5000/health"
+}
